d4a269667709ffab15d2fb962cc7eb5e8aca3c1a
Konstantin Vlasov <kevlasov@sbercloud.ru>
2024-12-07 17:20:12 +0300

    fix

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -3,19 +3,14 @@ from task_manager.users.models import User
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=150, blank=False,
-                            verbose_name=_('Name'))
-    description = models.TextField(blank=True,
-                                   verbose_name=_('Description'))
-    date_created = models.DateTimeField(auto_now_add=True,
-                                        verbose_name=_('Creation date'))
+    name = models.CharField(max_length=150, blank=False)
+    description = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     executor = models.ForeignKey(User, on_delete=models.CASCADE,
                                  blank=True, null=True,
-                                 default='', related_name='executors',
-                                 verbose_name=_('Executor'))
+                                 default='', related_name='executors')
     author = models.ForeignKey(User, on_delete=models.PROTECT,
-                               blank=False, related_name='authors',
-                               verbose_name=_('Author'))
+                               blank=False, related_name='authors')
 
 
     def __str__(self):
