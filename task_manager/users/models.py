64b925c135af473e60051eb6bede21534e0ca4d0
Konstantin Vlasov <kevlasov@sbercloud.ru>
2024-12-14 22:56:09 +0300

    fix

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
 )
-
+from django.db import models
 
 class User(AbstractBaseUser):
     username = models.CharField(max_length=150, unique=True)
@@ -25,6 +25,21 @@ class User(AbstractBaseUser):
     
 
 class UserManager(BaseUserManager):
+
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, password, **extra_fields)
     
     def get_by_natural_key(self, username):
         return self.get(username=username)
