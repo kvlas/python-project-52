d72c24514ef425deeef581c4d8d8f3da6763d5ef
Konstantin Vlasov <kevlasov@sbercloud.ru>
2024-12-14 21:16:01 +0300

    fix

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from task_manager.users.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from django.utils.translation import gettext_lazy as _
 
 class UserForm(UserCreationForm):
-    first_name = forms.CharField(max_length=150, required=True, label="first name")
-    last_name = forms.CharField(max_length=150, required=True, label="last name")
+    first_name = forms.CharField(max_length=150, required=True, label=_("First name"))
+    last_name = forms.CharField(max_length=150, required=True, label=_("Last name"))
 
 
     class Meta(UserCreationForm.Meta):
