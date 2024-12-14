734a386c6338a957a9ba9f37cadf9f190879bad2
Konstantin Vlasov <kevlasov@sbercloud.ru>
2024-12-14 22:59:05 +0300

    fix

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -4,25 +4,6 @@ from django.contrib.auth.models import (
 )
 from django.db import models
 
-class User(AbstractBaseUser):
-    username = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
-    is_active = models.BooleanField(default=True)
-
-    USERNAME_FIELD = 'username'
-
-    def __str__(self):
-        """Represent model object."""
-        return self.get_full_name
-    
-    def get_full_name(self):
-        full_name = f"{self.first_name} {self.last_name}".strip()
-        return full_name if full_name else self.identifier
-
-    def get_short_name(self):
-        return self.first_name or self.identifier
-    
 
 class UserManager(BaseUserManager):
 
@@ -43,3 +24,25 @@ class UserManager(BaseUserManager):
     
     def get_by_natural_key(self, username):
         return self.get(username=username)
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'username'
+
+    objects = UserManager()
+
+    def __str__(self):
+        """Represent model object."""
+        return self.get_full_name
+    
+    def get_full_name(self):
+        full_name = f"{self.first_name} {self.last_name}".strip()
+        return full_name if full_name else self.identifier
+
+    def get_short_name(self):
+        return self.first_name or self.identifier
+    
