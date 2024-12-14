a60cdfff5aa3f5e2f8a838e4ede2fec5ea2993f1
Konstantin Vlasov <kevlasov@sbercloud.ru>
2024-12-14 22:12:16 +0300

    fix

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
 from task_manager.users.models import User
 from task_manager.users.forms import UserForm
 
