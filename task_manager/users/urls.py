ab4b727cabb39586a687d945cd1897431106d850
Konstantin Vlasov <kevlasov@sbercloud.ru>
2024-12-14 23:40:48 +0300

    fix

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -4,8 +4,9 @@ from task_manager.users import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='users'),
+    path('<int:pk>/', views.UserView.as_view(), name='detail_user'),
     path('create/', views.UserCreateView.as_view(), name='create_user'),
     path('<int:pk>/update/', views.UserUpdateView.as_view(), name='update_user'),
     path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='delete_user'),
-    path('<int:pk>/', views.UserView.as_view(), name='detail_user'),
+
 ]
