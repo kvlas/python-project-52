a1359f3de2962087b4d81971ca3438e754f82d46
Konstantin Vlasov <kevlasov@sbercloud.ru>
2024-12-14 16:30:18 +0300

    fix

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import (
 
 
 class User(AbstractBaseUser):
+    identifier = models.CharField(max_length=40, unique=True)
+    USERNAME_FIELD = 'identifier'
 
     def __str__(self):
         """Represent model object."""
