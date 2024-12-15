9caa558354ff58ef108dd807e39899b447b465f7
Konstantin Vlasov <kevlasov@sbercloud.ru>
2024-12-15 21:37:08 +0300

    fix

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,8 +1,10 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.get_full_name()
