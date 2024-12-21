91d6a100ae0fa2204a59f62907e39df41f1da998
Konstantin Vlasov <kevlasov@sbercloud.ru>
2024-12-22 01:37:08 +0300

    fix

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -3,8 +3,24 @@ from django.urls import path
 from task_manager.statuses import views
 
 urlpatterns = [
-    path('', views.StatusListView.as_view(), name='statuses'),
-    path('create/', views.StatusCreateView.as_view(), name='create_status'),
-    path('<int:pk>/update/', views.StatusUpdateView.as_view(), name='update_status'),
-    path('<int:pk>/delete/', views.StatusDeleteView.as_view(), name='delete_status'),
+    path(
+        '', 
+        views.StatusListView.as_view(), 
+        name='statuses'
+    ),
+    path(
+        'create/', 
+        views.StatusCreateView.as_view(), 
+        name='create_status'
+    ),
+    path(
+        '<int:pk>/update/', 
+        views.StatusUpdateView.as_view(), 
+        name='update_status'
+    ),
+    path(
+        '<int:pk>/delete/', 
+        views.StatusDeleteView.as_view(), 
+        name='delete_status'
+    ),
 ]
