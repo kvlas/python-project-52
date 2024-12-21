fdd1986394777d66beca43e245f95005db30b0f5
Konstantin Vlasov <kevlasov@sbercloud.ru>
2024-12-21 22:41:55 +0300

    fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -116,20 +116,11 @@ DATABASES['default'].update(db_from_env)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {
             'min_length': 3,
         }
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    }
 ]
 
 # Custom user auth model
